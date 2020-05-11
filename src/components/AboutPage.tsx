cb2d1389f624b157eba3a7137738fe46e7d5f7ea
Ankan Mookherjee <tupaimook@gmail.com>
2020-05-11 17:45:07 +0100

    creating react components

---
+++ src/components/AboutPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>About</h2>
+        <h5>This app uses react </h5>
+      </>
+    );
+  }
+}
+
+export default AboutPage;
---
+++ src/components/HomePage.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+// function HomePage() {
+//   return (
+//     <div className="jumbotron">
+//       <h1>Pluralsight Administration</h1>
+//       <p>React, Flux and React Router for ultra-responsive web apps</p>
+//     </div>
+//   );
+// }
+
+const HomePage = () => {
+  return (
+    <div className="jumbotron">
+      <h1>Pluralsight Administration</h1>
+      <p>React, Flux and React Router for ultra-responsive web apps</p>
+      <a href="/about">About</a>
+    </div>
+  );
+};
+
+// const HomePage = () => (
+//   <div className="jumbotron">
+//     <h1>Pluralsight Administration</h1>
+//     <p>React, Flux and React Router for ultra-responsive web apps</p>
+//   </div>
+// );
+
+export default HomePage;
