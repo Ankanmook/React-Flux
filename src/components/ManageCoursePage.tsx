76d2f762f8d9f899b4bf5a76d56f33a09f391f32
Ankan Mookherjee <tupaimook@gmail.com>
2020-05-12 18:42:45 +0100

    error validation added

--- src/components/ManageCoursePage.tsx
+++ src/components/ManageCoursePage.tsx
@@ -5,6 +5,8 @@ import * as courseApi from "../api/courseApi";
 import { toast } from "react-toastify";
 
 const ManageCoursePage = (props: any) => {
+  const [errors, setErrors] = useState({});
+
   const [course, setCourse] = useState({
     id: null,
     slug: "",
@@ -29,8 +31,23 @@ const ManageCoursePage = (props: any) => {
     });
   }
 
+  function formIsValid() {
+    const _errors: any = {};
+
+    if (!course.title) _errors.title = "Title is required";
+    if (!course.authorId) _errors.authorId = "Author is required";
+    if (!course.category) _errors.category = "Category is required";
+
+    setErrors(_errors);
+    //Form is valid if object has no properties
+    return Object.keys(_errors).length === 0;
+  }
+
   function handleSubmit(event: any) {
     event.preventDefault();
+
+    if (!formIsValid()) return;
+
     courseApi.saveCourse(course).then(() => {
       props.history.push("/courses");
       toast.success("Course Saved");
@@ -43,6 +60,7 @@ const ManageCoursePage = (props: any) => {
       <Prompt when={true} message="Are your sure you want to leave?" />
       {props.match.params.slug}
       <CourseForm
+        errors={errors}
         course={course}
         onChange={handleChange}
         onSubmit={handleSubmit}
