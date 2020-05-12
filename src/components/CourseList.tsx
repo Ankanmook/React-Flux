561dc47081cf353189ce954c5d616f4fdd613a77
Ankan Mookherjee <tupaimook@gmail.com>
2020-05-12 09:44:42 +0100

    explicit declaration to course list prop type

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -27,7 +27,14 @@ function CourseList(props: CourseListProps) {
 }
 
 const CourseListPropType = {
-  courses: PropTypes.array.isRequired,
+  courses: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      authorId: PropTypes.number.isRequired,
+      category: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 type CourseListProps = InferProps<typeof CourseListPropType>;
