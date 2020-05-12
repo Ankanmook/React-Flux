c7c0f2db4b0065818fb56b8d48c9d327e5c3749d
Ankan Mookherjee <tupaimook@gmail.com>
2020-05-12 09:37:17 +0100

    playing with props

---
+++ src/components/CourseList.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes, { InferProps } from "prop-types";
+
+function CourseList(props: CourseListProps) {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Author Id</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.courses.map((course: any) => {
+          return (
+            <tr key={course.id}>
+              <td>{course.title}</td>
+              <td>{course.authorId}</td>
+              <td>{course.category}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+const CourseListPropType = {
+  courses: PropTypes.array.isRequired,
+};
+
+type CourseListProps = InferProps<typeof CourseListPropType>;
+
+// CourseList.propType = {
+//   courses: PropTypes.array.isRequired,
+// };
+
+export default CourseList;
